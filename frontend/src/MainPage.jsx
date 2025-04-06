5868b1a86981680a3f7e9dd28d295293a489b0ab
Ishika <ishikagilhotra17@gmail.com>
2025-04-07 04:46:53 +0530

    My DealStreet project commited

---
+++ frontend/src/MainPage.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import axios from "axios"; // Ensure axios is installed
+import Navbar from './NavBar.jsx';
+import './MainPage.css'
+
+const MainPage = () => {
+        return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default MainPage;
